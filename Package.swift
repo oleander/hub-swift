992427347e41e29f334c513b2cdbd605d171d56d
Linus Oleander <linus@oleander.nu>
2018-11-02 12:34:04 +0700

    Logger

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     .package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", from: "17.0.2"),
     .package(url: "https://github.com/tadija/AEXML.git", from: "4.1.0"),
     .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "4.0.2"),
-    .package(url: "https://github.com/oleander/logger-swift.git", from: "1.0.0"),
+    .package(url: "https://github.com/oleander/logger-swift.git", .branch("master")),
     .package(url: "https://github.com/Quick/Nimble.git", from: "7.0.2"),
     .package(url: "https://github.com/Quick/Quick.git", from: "1.2.0")
   ],
